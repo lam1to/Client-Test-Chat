69b837a7fca84d2f8635c0889adbe5e9717ba51d
lam1to <xtxtx96@mail.ru>
2023-06-28 22:37:00 +0300

    add loader and check user when refresh all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { BrowserRouter } from "react-router-dom";
 import AppRoute from "./components/Routing/AppRoute";
 import NavBar from "./components/NavBar";
 import "./styles/global.css";
+import { check } from "./http/user.services";
+import { useAppDispatch, useAppSelector } from "./Hooks/redux";
+import { UserSlice } from "./store/Reducers/UserSlice";
+import Loader from "./components/Loading/Loader";
 
 const App = () => {
+  const { user } = useAppSelector((state) => state.userReducer);
+  const { SetUser, SetIsAuth } = UserSlice.actions;
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    check()
+      .then((data) => {
+        dispatch(SetUser(data));
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <BrowserRouter>
       <NavBar />
